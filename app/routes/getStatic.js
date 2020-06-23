425de74dd5bc1036ad662b5db9910223394b27b9
Mac <mac@MacBook-Pro-Mac.local>
2020-06-23 21:51:10 +0300

    Добавил API рейтинг по возрастанию

--- app/routes/getStatic.js
+++ app/routes/getStatic.js
@@ -23,4 +23,58 @@ module.exports = (app, db) => {
             })
         })
     })
+
+  app.get('/api/get-statistic-raiting/user-raiting', (req, res) => {
+    db.query('SELECT user_login, glasses FROM wp_users ', (err, result) => {
+      if(err) throw err;
+      let ARRAY_SORT = [];
+      let format = new Promise((resolve) => {
+        result.forEach((e, index, arr) => {
+          e.glasses = Number(e.glasses);
+          ARRAY_SORT.push(e)
+          if (index == arr.length - 1) {
+            resolve()
+          }
+        })
+      })
+      
+      let i = 0;
+      // Сортируем 
+      new Promise(resolve => {
+        format.then(() => {
+          while (i < ARRAY_SORT.length) {
+            reverse(ARRAY_SORT);
+            i++;
+            if(i == ARRAY_SORT.length - 1) {
+              resolve()
+            }
+          }
+          
+        })
+      }).then((e) => {
+        res.send(ARRAY_SORT);
+      })
+      
+      
+     // console.log(ARRAY_SORT.length)
+    })
+    
+  })
+}
+
+const reverse = async (array) => {
+  array.forEach((el, index, arr) => {
+    if(array.length > (index + 1)) {
+     
+      if(array[index].glasses > array[index + 1].glasses) {
+          let v = array[index];
+          array[index] = array[index + 1];
+          array[index + 1] = v;
+      }
+      
+    }
+
+  })
+
+  return array;
 }
